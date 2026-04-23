961c6414f6a14dd8315a0e60ae4ad1db19b1aa7d
trevore777 <40551345+trevore777@users.noreply.github.com>
2026-04-23 14:21:10 +1000

    Force Puppeteer Chrome install on Render build

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -2,5 +2,8 @@ const { join } = require("path");
 
 /** @type {import("puppeteer").Configuration} */
 module.exports = {
-  cacheDirectory: join(__dirname, ".cache", "puppeteer")
+  cacheDirectory: join(__dirname, ".cache", "puppeteer"),
+  chrome: {
+    skipDownload: false
+  }
 };
