3befa50183f3aad02e5df742c9e4934687894cd7
trevore777 <40551345+trevore777@users.noreply.github.com>
2026-04-27 19:59:48 +1000

    Update server.js

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 /* BODY + COOKIE PARSING — MUST COME BEFORE ROUTES */
-aapp.use(express.urlencoded({ extended: true, limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(express.json({ limit: "10mb" }));
 app.use(express.json());
 app.use(cookieParser(process.env.APP_SECRET || "dev-secret"));
